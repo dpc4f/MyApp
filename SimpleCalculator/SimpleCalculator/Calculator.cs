3bc0c829fc9d6b7abd36b6f4a13ecc42996bb7a8
David Park <david.c4f@gmail.com>
2018-05-08 19:36:47 +0700

    Simple Calculator
    Simple calculator, command line usage

---
+++ SimpleCalculator/SimpleCalculator/Calculator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    /// <summary>
+    /// Will modify later (if possible) for big numbers which exceed the range of 64-bit
+    /// </summary>
+    class Calculator
+    { 
+        public const int ADD = 1;
+        public const int SUBTRACT = 2;
+        public const int MUL = 3;
+        public const int DIVIDE = 4;
+
+        public static double Calculate(double d1, double d2, int nOp)
+        {
+            double retVal = 0f;
+            switch (nOp)
+            {
+                case ADD:
+                    retVal = Add(d1, d2);
+                    break;
+                case SUBTRACT:
+                    retVal = Subtract(d1, d2);
+                    break;
+                case MUL:
+                    retVal = Mul(d1, d2);
+                    break;
+                case DIVIDE:
+                    retVal = Divide(d1, d2);
+                    break;
+                default:
+                    break;
+            }
+
+            return retVal;
+        }
+
+        private static double Add(double d1, double d2)
+        {
+            return d1 + d2;
+        }
+
+        private static double Subtract(double d1, double d2)
+        {
+            return d1 - d2;
+        }
+
+        private static double Mul(double d1, double d2)
+        {
+            return d1 * d2;
+        }
+
+        private static double Divide(double d1, double d2)
+        {
+            if (d2 == 0)
+                throw new Exception("Divided by zero");
+
+            return d1 / d2;
+        }
+    }
+}
---
+++ SimpleCalculator/SimpleCalculator/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---\t\t SIMPLE COMPUTATION APPLICATION \t\t---");
+            Console.WriteLine("1. Please specify the operator (1: Adding, 2: Subtracting, 3: Multiplying, 4: Dividing)");
+            Console.Write("\t");
+            string op = Console.ReadLine();
+
+            int nOp = 0;
+            try
+            {
+                nOp = Int32.Parse(op);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.ReadLine();
+
+                return;
+            }
+
+            if (nOp < 1 || nOp > 4)
+            {
+                Console.WriteLine("Invalid value input.");
+                Console.ReadLine();
+            }
+
+            double d1 = 0f;
+            double d2 = 0f;
+
+            try
+            {
+                Console.WriteLine("Input the 1st operand: ");
+                Console.Write("\t");
+                string operand1 = Console.ReadLine();
+                d1 = Double.Parse(operand1);
+
+                Console.WriteLine("Input the 2nd operand: ");
+                Console.Write("\t");
+                string operand2 = Console.ReadLine();
+                d2 = Double.Parse(operand2);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.ReadLine();
+
+                return;
+            }
+
+            Console.WriteLine("The result is: ");
+            Console.Write("\t");
+            Console.WriteLine(Calculator.Calculate(d1, d2, nOp));
+            Console.ReadLine();
+        }
+    }
+}
