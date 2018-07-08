e6f8c9368d0024af679c14bff34ad95e03fd92f5
David Park <david.c4f@gmail.com>
2018-07-08 16:31:02 +0700

    update sources

--- braces_syntax_checking/main.cpp
+++ braces_syntax_checking/main.cpp
@@ -22,62 +22,25 @@ std::set<char> set_opening = { '(', '{', '[' };
 std::set<char> set_closing = { ')', '}', ']' };
 
 string ex_one = "{ } [ ] ( )";
-string ex_two = "{ )";
-string ex_three = "{ ( } ) )";
+string ex_two = "{ } { [ ( ) ] } { { { ( ( ( [ ( ) ] ) ) ) } [ ( ) [ { } ] } }";
+string ex_three = "{ (  ) } ( ) ( [ { } ] )";
 string ex_four = "{";
-string ex_five = "{ } { [ ( ) ] }";
+string ex_five = "{ } { [ ( ) ] } { { { ( ( ( [ ( ) ] ) ) ) } ( ) [ { } ] } }";
 
 
 /*
-    select the first opening bracket
-    find next closing bracket
-    split the two string & validate each string
+    0. if the string is empty it is valid
+    1. select the first opening bracket, which is also the first character of the string
+    2. find next closet closing bracket at position pos
+    3. split the two strings & validate each string
+        3.1. if they are invalid, repeat step 2 with another next closest closing bracket
+        3.2. if they are valid, return true
+    4. return false (all cases are invalid)
 */
 
-
-
-bool is_opening(char b)
-{
-    return set_opening.count(b) > 0;
-}
-
-bool is_closing(char b)
-{
-    return set_closing.count(b) > 0;
-}
-
-bool check_validity(std::string str)
-{
-    if (str.length() == 0)
-        return true;
-    if (is_closing(str.front()) || is_opening(str.back()))
-        return false;
-
-    char b = str.front();
-    unsigned int len = str.length();
-    vector<size_t> v_pos;
-    size_t pos = 0;
-    while (pos != string::npos)
-    {
-        pos = str.find(map_braces[b]);
-        if (pos != string::npos)
-            v_pos.push_back(pos);
-    }
-
-    for (auto& pos : v_pos)
-    {
-        string str_one{""}, str_two{""};
-
-        if (pos >= 2)
-            str_one = str.substr(1, pos - 1);
-        if (pos < len - 1)
-            str_two = str.substr(pos + 1);
-        if (check_validity(str_one) && check_validity(str_two))
-            return true;
-    }
-
-    return false;
-}
+bool is_opening(const char &);
+bool is_closing(const char &);
+bool check_validity(const std::string&);
 
 int main()
 {
@@ -99,4 +62,51 @@ int main()
             << check_validity(ex_five) << endl;
 
     return 0;
+}
+
+
+bool is_opening(const char& b)
+{
+    return set_opening.count(b) > 0;
+}
+
+bool is_closing(const char& b)
+{
+    return set_closing.count(b) > 0;
+}
+
+bool check_validity(const std::string& str)
+{
+    if ( str.length() == 0 || (str.length() == 2 && str[1] == map_braces[str[0]]) )
+            return true;
+
+    if (is_closing(str.front()) || is_opening(str.back()))
+        return false;
+
+    const auto& b = str.front();
+    unsigned int len = str.length();
+    vector<size_t> v_pos;
+    size_t pos = std::string::npos;
+    do
+    {
+        pos = str.find(map_braces[b], pos+1);
+        if (pos == string::npos)
+            break;
+        v_pos.push_back(pos);
+    }
+    while (true);
+
+    for (auto& pos : v_pos)
+    {
+        string str_one{""}, str_two{""};
+
+        if (pos >= 2)
+            str_one = str.substr(1, pos - 1);
+        if (pos < len - 1)
+            str_two = str.substr(pos + 1);
+        if ( (check_validity(str_one) && check_validity(str_two)) == true )
+            return true;
+    }
+
+    return false;
 }
