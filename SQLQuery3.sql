ce107694f9fbfd7f46ef332e030b0793c250d98f
Duc Phan <davidpark.c4f@icloud.com>
2019-01-05 10:04:05 +0700

    use grid.mvc in mvc 5

--- SQLQuery3.sql
+++ SQLQuery3.sql
@@ -201,7 +201,8 @@ go
 
 create procedure sp_CreateStudentData
 	@MaxStudent int
-as
+  as
+begin
 	/*
 	begin
 		execute ('delete dbo.Students');
@@ -229,6 +230,7 @@ as
 	
 		set @idDept = @idDept + 1
 	end
+end
 go		
 
 execute dbo.sp_CreateStudentData 1500
@@ -250,11 +252,147 @@ execute sp_GetStudentSummary;
 go
 
 /*
-	CREATE CLASSES DATA TABLE
+	SUBJECTS TABLE
+*/
+-- Each department has 9 subjects for each students' seniority
+create procedure sp_CreateSubjectData
+	@MaxSubject int
+  as
+begin
+	declare @id int
+	declare @idStr varchar(50)
+	declare @idDept int
+	declare @idDeptStr varchar(50)
+	declare @idSeniority int
+	declare @idSeniorStr varchar(50)
+	declare @maxDept int
+	declare @maxSeniority int
+	declare @nCount	int
+	
+	set @idDept = 1
+	set @nCount = 1
+	set @maxDept = (select count(*) from dbo.Departments)
+	set @maxSeniority = (select count(*) from dbo.StudentSeniorities)
+	
+	while @idDept <= @maxDept
+	begin
+		set @idDeptStr = cast(@idDept as varchar)
+		set @idSeniority = 1
+		while @idSeniority <= @maxSeniority
+		begin
+			set @idSeniorStr = cast(@idSeniority as varchar)
+			set @id = 1
+			while @id <= @MaxSubject 
+			begin
+				set @idStr = cast(@id as varchar)
+				
+				insert into dbo.Subjects
+				values (@nCount, 'Subject ' + @idDeptStr + '.' + @idSeniorStr + '.' + @idStr, 3, @idDept, @idSeniority)
+				
+				set @id = @id + 1
+				set @nCount = @nCount + 1
+			end
+			set @idSeniority = @idSeniority + 1
+		end
+		set @idDept = @idDept + 1
+	end
+end
+go	
+
+drop procedure sp_CreateSubjectData;
+go
+
+exec sp_CreateSubjectData 5;
+go
+
+delete stmgmt.dbo.Subjects;
+go
+
+/*
+	LECTURERS TABLE
 */
 
+create procedure sp_CreateLecturerData
+	@maxLect int
+  as
+begin
+	declare @id int
+	declare @idStr varchar(50)
+	set @id = 1
+
+	while @id <= @maxLect
+	begin
+		set @idStr = cast(@id as varchar)
+
+		insert into stmgmt.dbo.Lecturers
+		values (@id, 'FName ' + @idStr, 'MName ' + @idStr, 'LName ' + @idStr)
+		set @id = @id + 1
+	end
+end
+go
+
+exec stmgmt.dbo.sp_CreateLecturerData 50
+go
+
+delete stmgmt.dbo.Lecturers;
+go
+/*
+	CLASSES TABLE
+*/
+
+alter table Classes
+alter column TaughtYear varchar not null;
+go
+
+alter table Classes
+alter column TaughtYear date not null;
+go
 --drop table if exists dbo.Classes
 --go
+
+-- if this year hasn't had a class for the subject, create two classes for each subject
+create procedure sp_CreateClassData
+  as
+begin
+	declare @id int
+	declare @idStr varchar(50)
+	declare @idDept int
+	declare @idDeptStr varchar(50)
+	declare @idSeniority int
+	declare @idSeniorStr varchar(50)
+	declare @maxDept int
+	declare @maxSeniority int
+	declare @nCount	int
+	
+	set @idDept = 1
+	set @nCount = 1
+	set @maxDept = (select count(*) from dbo.Departments)
+	set @maxSeniority = (select count(*) from dbo.StudentSeniorities)
+	
+	while @idDept <= @maxDept
+	begin
+		set @idDeptStr = cast(@idDept as varchar)
+		set @idSeniority = 1
+		while @idSeniority <= @maxSeniority
+		begin
+			set @idSeniorStr = cast(@idSeniority as varchar)
+			set @id = 1
+			while @id <= @MaxSubject 
+			begin
+				set @idStr = cast(@id as varchar)
+				
+				insert into dbo.Subjects
+				values (@nCount, 'Subject ' + @idDeptStr + '.' + @idSeniorStr + '.' + @idStr, 3, @idDept, @idSeniority)
+				
+				set @id = @id + 1
+				set @nCount = @nCount + 1
+			end
+			set @idSeniority = @idSeniority + 1
+		end
+		set @idDept = @idDept + 1
+	end
+end
+go	
 
 
 /*
--- StudentManagement.v0.03/StudentManagement.v0.03/App_Start/BundleConfig.cs
+++ StudentManagement.v0.03/StudentManagement.v0.03/App_Start/BundleConfig.cs
@@ -25,6 +25,13 @@ namespace StudentManagement.v0._03
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/datatables").Include(
+                        "~/Scripts/DataTables/jquery.dataTables.min.js",
+                        "~/Scripts/DataTables/dataTables.bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/datatables").Include(
+                        "~/Content/DataTables/css/dataTables.bootstrap.css"));
         }
     }
 }
