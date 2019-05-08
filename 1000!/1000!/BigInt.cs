1be9fe7a303a74fe665c7111e5d15f9587853562
Duc Phan <davidpark.c4f@icloud.com>
2019-05-09 06:21:27 +0700

    - add source(s)

---
+++ 1000!/1000!/BigInt.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1000_
+{
+    /*
+     *          123
+        	     23
+            --------
+                369	 
+               246
+            --------
+               2829
+     * */
+    class BigInt
+    {
+        public string s;
+
+        public BigInt(string input)
+        {
+            // check valid string
+            if (input.All(Char.IsDigit) == false)
+                return;
+
+            // assign the value
+            s = input;
+        }
+
+        public BigInt Add(BigInt bi)
+        {
+            if (s.Length < bi.s.Length)
+            {
+                ZeroLeftPadding(bi.s.Length - s.Length);
+            }
+            if (s.Length > bi.s.Length)
+            {
+                bi.ZeroLeftPadding(s.Length - bi.s.Length);
+            }
+
+            string ret = "";
+            int r, e, t, u;
+            e = 0;
+            for (int i=s.Length-1; i>=0; --i)
+            {
+                Int32.TryParse(s[i].ToString(), out t);
+                Int32.TryParse(bi.s[i].ToString(), out u);
+                r = t + u + e;
+                e = r / 10;
+                r = r % 10;
+                ret = r.ToString() + ret;
+            }
+            if (e > 0) ret = e.ToString() + ret;
+
+            return new BigInt(ret);
+        }
+
+        public BigInt Multiple(int n)
+        {
+            string ret = "";
+            int r, e, t;
+            e = 0;
+            for (int i=s.Length-1; i>=0; --i)
+            {
+                
+                Int32.TryParse(s[i].ToString(), out t);
+                r = t * n + e;
+                e = r / 10;
+                r = r % 10;
+                ret = r.ToString() + ret;
+            }
+            if (e > 0) ret = e.ToString() + ret;
+
+            return new BigInt(ret);
+        }
+
+        public BigInt Multiple(BigInt bi)
+        {
+            BigInt[] middle_result = new BigInt[s.Length];
+            for (int i=s.Length-1; i>=0; --i)
+            {
+                int n = 0;
+                Int32.TryParse(s[i].ToString(), out n);
+                middle_result[i] = bi.Multiple(n);
+                middle_result[i].ZeroRightPadding(s.Length - 1 - i);
+            }
+            BigInt ret = new BigInt("0");
+            for (int i=0; i<s.Length; ++i)
+            {
+                ret = ret.Add(middle_result[i]);
+            }
+
+            return ret;
+        }
+
+        public void ZeroRightPadding(int n)
+        {
+            for (int i=0; i<n; ++i)
+            {
+                s += "0";
+            }
+        }
+
+        public void ZeroLeftPadding(int n)
+        {
+            for (int i=0; i<n; ++i)
+            {
+                s = "0" + s;
+            }
+        }
+
+        public void PrintValue()
+        {
+            int k = s.Length % 3;
+            for (int i = 0; i < s.Length-1; ++i)
+            {
+                Console.Write(s[i]);
+                if ((i + 1 - k) % 3 == 0) Console.Write(',');
+            }
+            Console.Write(s[s.Length - 1]);
+        }
+    }
+}
---
+++ 1000!/1000!/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1000_
+{
+    class Program
+    {
+        static int N_MAX = 1000;
+        static void Main(string[] args)
+        {
+            BigInt[] bigInts = new BigInt[N_MAX];
+            for (int i = 0; i < N_MAX; ++i)
+                bigInts[i] = new BigInt((i + 1).ToString());
+            BigInt ret = bigInts[0];
+            for (int i = 1; i < N_MAX; ++i)
+            {
+                ret = ret.Multiple(bigInts[i]);
+            }
+            ret.PrintValue();
+
+            Console.ReadLine();
+        }
+    }
+}
